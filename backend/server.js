277de0d5fc2d5d4e9ccda54d2e355980faae05a8
prkr-28 <priyanshu402muz@gmail.com>
2025-08-15 19:44:43 +0530

    completed

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import { connectDB } from './config/db.js';
-import userRouter from './routes/userRoutes.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import resumeRoutes from './routes/resumeRoutes.js';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import { connectDB } from "./config/db.js";
+import userRouter from "./routes/userRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import resumeRoutes from "./routes/resumeRoutes.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -20,24 +20,27 @@ connectDB();
 //middleware
 app.use(express.json());
 
-app.use('/api/auth', userRouter);
-app.use('/api/resume', resumeRoutes);
+app.use("/api/auth", userRouter);
+app.use("/api/resume", resumeRoutes);
 
 app.use(
-  '/uploads',
-  express.static(path.join(__dirname, 'uploads'), {
+  "/uploads",
+  express.static(path.join(__dirname, "uploads"), {
     setHeaders: (res, _path) => {
-      res.set('Access-Control-Allow-Origin', 'http://localhost:5173');
+      res.set(
+        "Access-Control-Allow-Origin",
+        "https://resume-xpert-ochre.vercel.app"
+      );
     },
   })
 );
 
 //routes
 
-app.get('/', (req, res) => {
-  res.send('api is working');
+app.get("/", (req, res) => {
+  res.send("api is working");
 });
 
 app.listen(port, () => {
-  console.log('server started');
+  console.log("server started");
 });
--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,15 +1,15 @@
-export const BASE_URL = 'http://localhost:4000';
+export const BASE_URL = "https://resumexpert-uyh9.onrender.com";
 
 //routes for frontend...
 export const API_PATHS = {
   AUTH: {
-    REGISTER: '/api/auth/register',
-    LOGIN: '/api/auth/login',
-    GET_PROFILE: '/api/auth/profile',
+    REGISTER: "/api/auth/register",
+    LOGIN: "/api/auth/login",
+    GET_PROFILE: "/api/auth/profile",
   },
   RESUME: {
-    CREATE: '/api/resume',
-    GET_ALL: '/api/resume',
+    CREATE: "/api/resume",
+    GET_ALL: "/api/resume",
     GET_BY_ID: (id) => `/api/resume/${id}`,
 
     UPDATE: (id) => `/api/resume/${id}`,
@@ -17,6 +17,6 @@ export const API_PATHS = {
     UPLOAD_IMAGES: (id) => `/api/resume/${id}/upload-images`,
   },
   IMAGE: {
-    UPLOAD_IMAGE: 'api/auth/upload-image',
+    UPLOAD_IMAGE: "api/auth/upload-image",
   },
 };
